b859b94105c899aac8352ba6043fd783e36260cc
Jacob Loris <jacobloris@outlook.com>
2021-04-17 21:11:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,13 +5,13 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2] #Top row 
-  [3, 4, 5] #Middle Row 
-  [6, 7, 8] #Bottom Row
-  [0, 4, 8] #Diagonal 
-  [2, 4, 6] #Diagonal
-  [0, 3, 6] #Vertical row 1 
-  [1, 4, 7] #Vertical Row 2 
+  [0, 1, 2], #Top row 
+  [3, 4, 5], #Middle Row 
+  [6, 7, 8], #Bottom Row
+  [0, 4, 8], #Diagonal 
+  [2, 4, 6], #Diagonal
+  [0, 3, 6], #Vertical row 1 
+  [1, 4, 7], #Vertical Row 2 
   [2, 5, 8] #Vertical Row 3
   ]
 
